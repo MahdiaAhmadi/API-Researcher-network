ec57e7b07bd8e1e900f3a736862a4f0b55d70788
jgmsilva <jgui.silva1510@gmail.com>
2024-04-15 12:25:23 +0100

    quick fix

--- users_service.py
+++ users_service.py
@@ -17,37 +17,37 @@ UserRouter = APIRouter()
 
 @UserRouter.get("/users-list")
 async def list_users():
-    documents = getAll(users_collection)
+    documents = await getAll(users_collection)
     return ResponseModel(documents, "List of all users")
 
 
 @UserRouter.get("/user-type-list")
 async def list_usertype():
-    documents = getAll(usertype_collection)
+    documents = await getAll(usertype_collection)
     return ResponseModel(documents, "List of all available user types")
 
 @UserRouter.post("/create-user")
 async def create_user(user: User):
-    response = addOne(users_collection, user.model_dump())
+    response = await addOne(users_collection, user.model_dump())
     return ResponseModel(response, "User was created")
 
 @UserRouter.get("/find-user-by-id/{user_id}")
 async def read_item(user_id: str):
-    item = getOne(users_collection, user_id)
+    item = await getOne(users_collection, user_id)
     if item:
         return ResponseModel(item, "Found user")
     return ErrorResponseModel("Error occurred", 404, "user does not exist")
 
 @UserRouter.put("/update-user/{user_id}")
 async def update_item(user_id: str, user: User):
-    updated_user = updateOne(users_collection, user_id, user.model_dump())
+    updated_user = await updateOne(users_collection, user_id, user.model_dump())
     if updated_user:
         return ResponseModel({"id": user_id}, "User sucessfully updated")
     return ErrorResponseModel("Error occurred", 404, "user does not exist")
 
 @UserRouter.delete("/delete-user/{user_id}")
 async def delete_user(user_id: str):
-    deleted_user = deleteOne(users_collection, user_id)
+    deleted_user = await deleteOne(users_collection, user_id)
     if deleted_user:
         return ResponseModel({"id": user_id}, "User sucessfully deleted")
     return ErrorResponseModel("Error occurred", 404, "user does not exist")
