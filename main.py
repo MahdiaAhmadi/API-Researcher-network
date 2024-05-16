d2f95510c3f393bed7ecf2b1d9a4e32fb78a0b76
MahdiaAhmadi <Mahdia@example.com>
2024-05-16 12:14:35 +0100

    cross middle ware imported

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ from users_service import UserRouter
 from posts_service import PostRouter
 from comments_service import CommentRouter
 from categories_service import CategoryRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= ["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(InstitutionRouter, tags=["Institution"], prefix="/institution")
 app.include_router(UserRouter, tags=["User"], prefix="/user")
 app.include_router(PostRouter, tags=["Post"], prefix="/post")
